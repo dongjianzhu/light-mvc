10dafecfcf2af9d2a53206ae26be9ad6696637f5
light.wind <light.wind@28cad30a-22a4-11df-b7b2-c9b5fbd24121>
2010-08-18 15:18:02 +0000

    share maven projects

---
+++ org.lightframework.mvc/org.lightframework.mvc.core/src/main/java/org/lightframework/mvc/clazz/ClazzLoader.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lightframework.mvc.clazz;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+/**
+ * TODO : document me
+ * 
+ * @author fenghm (live.fenghm@gmail.com)
+ * @since 1.0.0
+ */
+public class ClazzLoader extends java.lang.ClassLoader {
+
+	private Class<?> callingClass;
+
+	public ClazzLoader() {
+
+	}
+
+	public ClazzLoader(ClazzLoader parent) {
+		super(parent);
+	}
+
+	public ClazzLoader(ClazzLoader parent, Class<?> callingClass) {
+		this.callingClass = callingClass;
+	}
+
+	public ClazzLoader(Class<?> callingClass) {
+		this.callingClass = callingClass;
+	}
+
+	@Override
+	public final Class<?> loadClass(String name) throws ClassNotFoundException {
+		Class<?> foundClass = null;
+
+		ClassLoader loader1 = Thread.currentThread().getContextClassLoader();
+		if (null != loader1 && !loader1.equals(this)) {
+			foundClass = loadClass(name, loader1);
+		}
+
+		if (null == foundClass && null != callingClass) {
+			ClassLoader loader2 = callingClass.getClassLoader();
+			if (!loader2.equals(loader1)) {
+				foundClass = loadClass(name, loader2);
+			}
+
+			if (null == foundClass) {
+				ClassLoader loader3 = ClazzLoader.class.getClassLoader();
+				if (!loader3.equals(loader2) && !loader3.equals(loader1)) {
+					foundClass = loadClass(name, loader3);
+				}
+			}
+		}
+		
+		if(null == foundClass){
+			foundClass = findClass(name);
+		}
+
+		return foundClass;
+	}
+	
+	@Override
+    public URL getResource(String name) {
+		URL url = null;
+
+		ClassLoader loader1 = Thread.currentThread().getContextClassLoader();
+		if (null != loader1 && !loader1.equals(this)) {
+			url = getResource(name,loader1);
+		}
+
+		if (null == url && null != callingClass) {
+			ClassLoader loader2 = callingClass.getClassLoader();
+			if (!loader2.equals(loader1)) {
+				url = getResource(name,loader2);
+			}
+
+			if (null == url) {
+				ClassLoader loader3 = ClazzLoader.class.getClassLoader();
+				if (!loader3.equals(loader2) && !loader3.equals(loader1)) {
+					url = getResource(name,loader3);
+				}
+			}
+		}
+		
+		if(null == url && !name.startsWith("/")){
+			url = getResource("/" + name);
+		}
+
+		return url;
+    }
+
+	@Override
+	public Enumeration<URL> getResources(String name) throws IOException {
+		//XXX : just call Thread.currentThread().getContextClassLoader()
+		return Thread.currentThread().getContextClassLoader().getResources(name);
+	}
+
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		throw new ClassNotFoundException("class '" + name + "' not found");
+	}
+	
+	private Class<?> loadClass(String name, ClassLoader loader) throws ClassNotFoundException {
+		if (loader.getClass().isAssignableFrom(this.getClass())) {
+			return ((ClazzLoader) loader).findClass(name);
+		} else {
+			try {
+				return loader.loadClass(name);
+			} catch (ClassNotFoundException e) {
+				return null;
+			}
+		}
+	}
+	
+	private URL getResource(String name,ClassLoader loader) {
+		if (loader.getClass().isAssignableFrom(this.getClass())) {
+			return ClazzLoader.class.getResource(name);
+		} else {
+			return loader.getResource(name);
+		}
+	}
+}
---
+++ org.lightframework.mvc/org.lightframework.mvc.core/src/main/java/org/lightframework/mvc/render/json/JSON.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lightframework.mvc.render.json;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Map;
+
+import org.lightframework.mvc.render.IRenderContext;
+import org.lightframework.mvc.render.DataRender;
+
+/**
+ * json encoder
+ * 
+ * @author fenghm (live.fenghm@gmail.com)
+ * 
+ * @since 1.0.0
+ */
+public class JSON {
+
+	private static final JSONRender  render  = new JSONRender();
+	private static final JSONContext context = new JSONContext();
+	private static final JSONDecoder decoder = new JSONDecoder(false);
+
+	public static String encode(Object value) {
+		return render.encode(value, context);
+	}
+
+	public static String encode(Object value, IRenderContext context) {
+		return render.encode(value, context);
+	}
+
+	public static JSONObject decode(Reader reader) {
+		try {
+			StringBuilder buf = new StringBuilder();
+			char[] cbuf = new char[32];
+			int c;
+			while ((c = reader.read(cbuf)) >= 0) {
+				buf.append(cbuf, 0, c);
+			}
+			return decode(buf.toString());
+		} catch (IOException e) {
+			throw new JSONException("error reading json from reader : " + e.getMessage(), e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static JSONObject decode(String source) {
+		return new JSONObject(decoder.decode(source, Map.class));
+	}
+
+	private static final class JSONRender extends DataRender {
+
+	}
+}
